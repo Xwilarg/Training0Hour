506079a751840c1af1be12fc8a9be61ff3d200e9
Xwilarg <xwilarg@yahoo.fr>
2021-10-31 00:11:19 +0200

    Add spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _prefab;
+
+    List<GameObject> spawned = new();
+
+    private void Start()
+    {
+        Spawn();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        if (spawned.Count > 0)
+        {
+            spawned.Last().GetComponent<PlayerController>().IsCurrent = false;
+        }
+
+        var go = Instantiate(_prefab, transform);
+        go.transform.position = transform.position;
+        spawned.Add(go);
+    }
+}
